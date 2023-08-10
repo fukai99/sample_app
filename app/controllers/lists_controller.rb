9008c9143a518d82996b09c8c86ed0d1048e271c
fukai99 <141290272+fukai99@users.noreply.github.com>
2023-08-10 10:32:27 +0000

    chapter9

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,13 +4,14 @@ class ListsController < ApplicationController
   end
   
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to list_path(list.id)
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end  
   end
+  
   def index
     @lists = List.all
   end
@@ -19,9 +20,25 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
-  def edit
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
   end
+  
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+  end
+  
+  private
   def list_params
-    params.require(:list).permit(:title, :body)
+    
+    params.require(:list).permit(:title, :body, :image)
   end
 end
